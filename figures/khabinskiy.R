1605d79aaa05ac30887ccde6a53f5e639e19b131
Dennis Evangelista <devangel77b@gmail.com>
2026-02-16 18:36:42 -0500

    blah

--- figures/khabinskiy.R
+++ figures/khabinskiy.R
@@ -4,6 +4,7 @@ library(dplyr)
 raw <- read.csv('velocity.csv',header=TRUE)
 grouped <- dplyr::group_by(raw,type)
 
+# make plot
 fig <- ggplot(raw, aes(t, v_ms, colour=type)) +
     geom_hline(yintercept=0) +
     geom_point() +
@@ -12,6 +13,13 @@ fig <- ggplot(raw, aes(t, v_ms, colour=type)) +
     ylab('$v$, \\unit{\\meter\\per\\second}')+
     theme_bw()
 
+# print stats for linear fits
+print(summary(lm(v_ms~t,filter(raw,type=='water'))))
+print(summary(lm(v_ms~t,filter(raw,type=='rocks'))))
+
+# use ANOVA to check if differences are significant
 model1 <- lm(v_ms~t, data=raw)
 model2 <- lm(v_ms~t+type, data=raw)
 print(anova(model1,model2))
+
+

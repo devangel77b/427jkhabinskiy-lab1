d2e8c15deb35d0f8a1af91c84f27660eb074ac02
Dennis Evangelista <devangel77b@gmail.com>
2026-02-16 19:11:48 -0500

    blah

--- figures/khabinskiy.R
+++ figures/khabinskiy.R
@@ -6,12 +6,20 @@ grouped <- dplyr::group_by(raw,type)
 
 # make plot
 fig <- ggplot(raw, aes(t, v_ms, colour=type)) +
-    geom_hline(yintercept=0) +
+    geom_hline(yintercept=0,colour="gray50") +
     geom_point() +
-    geom_smooth(method="lm",se=FALSE)+
+    geom_smooth(method="lm",se=FALSE) +
+    theme_classic(base_size=8) + 
     xlab('$t$, \\unit{\\second}')+
     ylab('$v$, \\unit{\\meter\\per\\second}')+
-    theme_bw()
+    theme(legend.position="inside",
+	legend.position.inside=c(0.95,0.95),
+	legend.justification.inside=c("right","top"),
+	legend.title=element_blank(),
+	axis.title.y=element_text(margin=margin(t=0,r=5,b=0,l=0,unit="pt")))
+
+# save the figure
+ggsave("fig2.svg", plot=fig, width=3.4167, height=2, units="in")
 
 # print stats for linear fits
 print(summary(lm(v_ms~t,filter(raw,type=='water'))))
